110d989b2e978954f2484179e8f0ce1167540569
Goldenstone <hxljustdoit@gmail.com>
2014-08-19 22:19:47 +0800

    hi

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var app = express();
 var wechat = require('wechat');
 
 // view engine setup
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -24,42 +25,6 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-app.use('/hello', hello);
-app.use('/weixin', access);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
 
 app.use(express.query()); // Or app.use(express.query());
 app.use('/weixin', wechat('hellonode', function (req, res, next) {
@@ -98,9 +63,44 @@ app.use('/weixin', wechat('hellonode', function (req, res, next) {
   }
 }));
 
+app.use('/', routes);
+app.use('/users', users);
+app.use('/hello', hello);
+app.use('/weixin', access);
 
-var port = Number(process.env.PORT || 3000);
-app.listen(port);
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+app.listen(app.get('port'));
 
 app.enable('trust proxy');
 
