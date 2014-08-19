33c2a28a542e9d6a8ced439c38bac6dc3c93a151
Goldenstone <hxljustdoit@gmail.com>
2014-08-20 00:01:21 +0800

    menu

--- routes/weixin.js
+++ routes/weixin.js
@@ -1,4 +1,29 @@
 var wechat = require('wechat');
+var API = require('wechat').API;
+var api = new API('wx555b5022e7ce7cd0', '81f42087bb0cf20de841d0e3e411f6fc');
+
+var menu = {
+ "button":[
+   {
+     "type":"click",
+     "name":"今日歌曲",
+     "key":"V1001_TODAY_MUSIC"
+   },
+   {
+     "name":"菜单",
+     "sub_button":[
+       {
+         "type":"view",
+         "name":"搜索",
+         "url":"http://www.soso.com/"
+       },
+       {
+         "type":"click",
+         "name":"赞一下我们",
+         "key":"V1001_GOOD"
+       }]
+   }]
+};
 
 exports.weixin = wechat('hellonode', function(req, res, next) {
   var message = req.weixin;
@@ -7,4 +32,7 @@ exports.weixin = wechat('hellonode', function(req, res, next) {
   } else {
     res.reply('hehe');
   }
+  api.createMenu(menu, function() {
+
+  });
 });
