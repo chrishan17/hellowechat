6923c232e7415031855455a1142cec7d1f3841d3
Goldenstone <hxljustdoit@gmail.com>
2014-08-19 23:28:52 +0800

    hi

--- routes/weixin.js
+++ routes/weixin.js
@@ -2,5 +2,9 @@ var wechat = require('wechat');
 
 exports.weixin = wechat('hellonode', function(req, res, next) {
   var message = req.weixin;
-  res.reply('hehe');
+  if (message.Content === 'hi') {
+    res.reply('hi');
+  } else {
+    res.reply('hehe');
+  }
 });
